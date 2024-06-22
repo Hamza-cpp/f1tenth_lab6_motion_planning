cadceefb9d55855c10435a9ac663d5a19598779f
hamza-cpp <hamza.okhadir2018@gmail.com>
2024-06-22 10:55:47 +0100

    📝 chore: Update package information and maintainer details

--- lab7_pkg/setup.py
+++ lab7_pkg/setup.py
@@ -7,16 +7,16 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
+        ('share/ament_index/resource_index/packages',   
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='zzangupenn',
-    maintainer_email='zzang@seas.upenn.edu',
-    description='f1tenth lab7_pkg',
-    license='TODO: License declaration',
+    maintainer='OKHADIR Hamza',
+    maintainer_email='hamza.okhadir2018@gmail.com',
+    description='My solution for Lab 6: Motion Planning RRT for University of Pennsylvania course F1TENTH',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
